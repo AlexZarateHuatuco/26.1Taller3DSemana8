331ad8356452f20e0282f2a0699a0457cfdd51e8
ZeroAndDonuts <gianmarco07042004@gmail.com>
2026-05-21 11:11:46 -0500

    Escena Spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,21 +6,21 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject prefabSpawn;
-    public int numberSpawner = 10;
+    public int numberSpawn = 10;
 
     public int SpawnerX = 10;
     public int SpawnerY = 0;
     public int SpawnerZ = 10;
     void Start()
     {
-        for (int i = 0; i < numberSpawner; i++)
+        for (int i = 0; i < numberSpawn; i++)
         {
             Spawn();
         }
     }
     void Spawn()
     {
-        Vector3 randomPosition = new Vector3(Random.Range(-SpawnerX, SpawnerX), Random.Range(-SpawnerY, SpawnerY), Random.Range(-SpawnerZ, SpawnerZ));
+        Vector3 randomPosition = new Vector3(Random.Range(-SpawnerX, SpawnerX), Random.Range(-SpawnerY, SpawnerY), Random.Range(-SpawnerZ, SpawnerZ)) + transform.position;
         GameObject clone = Instantiate(prefabSpawn, randomPosition, Quaternion.identity);
     }
 }
